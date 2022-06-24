39edf97e5b51d23e21a5f30445a3810f9518a80c
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-24 17:34:47 +0300

    Classes renamed

---
+++ CleanCode_ExampleTask15.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Player { }
+public class Gun { }
+public class Follower { }
+
+public class Units
+{
+    public IReadOnlyCollection<Unit> Collection { get; private set; }
+}
+public class Unit { }
