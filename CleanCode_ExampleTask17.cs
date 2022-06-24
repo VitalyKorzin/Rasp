d8fc6b7f0436642c6fbcca7133388b0a8230b114
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-24 17:45:06 +0300

    Methods renamed

---
+++ CleanCode_ExampleTask17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class CleanCode_ExampleTask17
+{
+    public static void CreateNewObject()
+    {
+        //Создание объекта на карте
+    }
+
+    public static void GenerateChance()
+    {
+        _chance = Random.Range(0, 100);
+    }
+
+    public static int CalculateSalary(int hoursWorked)
+    {
+        return _hourlyRate * hoursWorked;
+    }
+}
