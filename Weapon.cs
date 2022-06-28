3487569cec4c239f3ccbd6acc9b7eb830145859a
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-28 03:04:26 +0300

    Refactored

--- Weapon.cs
+++ Weapon.cs
@@ -2,54 +2,60 @@ using System;
 
 public struct Weapon
 {
+    private readonly int _bulletsPerShot;
     private readonly int _damage;
     private int _bullets;
 
-    public Weapon(int damage, int bullets)
+    public Weapon(int damage, int bullets, int bulletsPerShot)
     {
-        if (damage < 0)
-            throw new ArgumentOutOfRangeException();
+        if (damage <= 0)
+            throw new ArgumentOutOfRangeException(nameof(damage));
 
-        if (bullets < 0)
-            throw new ArgumentOutOfRangeException();
+        if (bullets <= 0)
+            throw new ArgumentOutOfRangeException(nameof(bullets));
+
+        if (bulletsPerShot <= 0)
+            throw new ArgumentOutOfRangeException(nameof(bulletsPerShot));
 
         _damage = damage;
         _bullets = bullets;
+        _bulletsPerShot = bulletsPerShot;
     }
 
-    public void Fire(IDamageable target)
+    public void Fire(Player target)
     {
-        if (_bullets > 0)
-        {
+        if (target == null)
+            throw new ArgumentNullException(nameof(target));
+
+        if (target.Alive)
             target.ApplyDamage(_damage);
-            _bullets--;
-        }
+
+        _bullets -= _bulletsPerShot;
     }
+
+    public bool CanFire() => _bullets >= _bulletsPerShot;
 }
 
-public interface IDamageable 
-{
-    public void ApplyDamage(int damage);
-}
-
-public class Player : IDamageable
+public class Player
 {
     private int _health;
 
     public Player(int health)
     {
-        if (health < 0)
-            throw new ArgumentOutOfRangeException();
+        if (health <= 0)
+            throw new ArgumentOutOfRangeException(nameof(health));
 
         _health = health;
     }
 
+    public bool Alive => _health > 0;
+
     public void ApplyDamage(int damage)
     {
-        if (damage < 0)
-            throw new ArgumentOutOfRangeException();
+        if (damage <= 0)
+            throw new ArgumentOutOfRangeException(nameof(damage));
 
-        if (_health - damage <= 0)
+        if (_health < damage)
             _health = 0;
         else
             _health -= damage;
@@ -63,6 +69,9 @@ public class Bot
     public Bot(Weapon weapon) 
         => _weapon = weapon;
 
-    public void OnSeePlayer(Player player) 
-        => _weapon.Fire(player);
+    public void OnSeePlayer(Player player)
+    {
+        if (_weapon.CanFire)
+            _weapon.Fire(player);
+    }
 }
