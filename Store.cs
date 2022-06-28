e076ed5adee96b7a4c73001942950fe83a9f26f0
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-28 03:32:10 +0300

    Refactored

--- Store.cs
+++ Store.cs
@@ -9,28 +9,35 @@ public class Store
         Good iPhone12 = new Good("IPhone 12");
         Good iPhone11 = new Good("IPhone 11");
 
-        Warehouse warehouse = new Warehouse();
+        var warehouse = new Warehouse();
 
-        Shop shop = new Shop(warehouse);
+        var shop = new Shop(warehouse);
 
         warehouse.Delive(iPhone12, 10);
         warehouse.Delive(iPhone11, 1);
 
         //Вывод всех товаров на складе с их остатком
-        foreach (var cell in warehouse.Cells)
-            Console.WriteLine($"{cell.Good} - {cell.Count}");
+        ShowGoods(warehouse.Cells);
 
         Cart cart = shop.Cart();
         cart.Add(iPhone12, 4);
         cart.Add(iPhone11, 3); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
 
         //Вывод всех товаров в корзине
-        foreach (var cell in cart.Cells)
-            Console.WriteLine($"{cell.Good} - {cell.Count}");
+        ShowGoods(cart.Cells);
 
         Console.WriteLine(cart.Order().Paylink);
 
         cart.Add(iPhone12, 9); //Ошибка, после заказа со склада убираются заказанные товары
+    }
+
+    public static void ShowGoods(IReadOnlyList<IReadOnlyCell> cells)
+    {
+        if (cells == null)
+            throw new ArgumentNullException(nameof(cells));
+
+        foreach (var cell in cells)
+            Console.WriteLine($"{cell.Good} - {cell.Count}");
     }
 }
 
@@ -60,7 +67,7 @@ public class Warehouse
 
     public void Delive(Good good, int count)
     {
-        if (count < 0)
+        if (count <= 0)
             throw new ArgumentOutOfRangeException(nameof(count));
 
         var newCell = new Cell(good, count);
@@ -73,13 +80,23 @@ public class Warehouse
 
     public void Order(Good good, int count)
     {
-        if (count < 0)
+        if (count <= 0)
             throw new ArgumentOutOfRangeException(nameof(count));
 
         if (TryFindCellIndex(good, out int cellIndex))
             _cells[cellIndex] = _cells[cellIndex].RemoveGoods(count);
         else
             throw new InvalidOperationException();
+    }
+
+    public bool CanOrder(Good good, int count)
+    {
+        if (count <= 0)
+            throw new ArgumentOutOfRangeException(nameof(count));
+
+        var foundCell = _cells.FirstOrDefault(cell => cell.Good.Name == good.Name);
+
+        return foundCell != null && foundCell.Count >= count;
     }
 
     private bool TryFindCellIndex(Good good, out int cellIndex)
@@ -99,7 +116,7 @@ public class Cell : IReadOnlyCell
 {
     public Cell(Good good, int count)
     {
-        if (count < 0)
+        if (count <= 0)
             throw new ArgumentOutOfRangeException(nameof(count));
 
         Good = good;
@@ -122,7 +139,7 @@ public class Cell : IReadOnlyCell
 
     public Cell RemoveGoods(int count)
     {
-        if (count < 0)
+        if (count <= 0)
             throw new ArgumentOutOfRangeException(nameof(count));
 
         if (count > Count)
@@ -151,10 +168,14 @@ public class Cart
 
     public void Add(Good good, int count)
     {
-        if (count < 0)
+        if (count <= 0)
             throw new ArgumentOutOfRangeException(nameof(count));
 
-        _warehouse.Order(good, count);
+        if (_warehouse.CanOrder(good, count))
+            _warehouse.Order(good, count);
+        else
+            throw new InvalidOperationException();
+
         var newCell = new Cell(good, count);
 
         if (TryFindCellIndex(good, out int cellIndex))
@@ -165,7 +186,8 @@ public class Cart
 
     public void Cancel()
     {
-        if (Empty) return;
+        if (Empty) 
+            return;
 
         foreach (var cell in _cells)
             _warehouse.Delive(cell.Good, cell.Count);
