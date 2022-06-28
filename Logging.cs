fdf11437dc6365e0adf35ff99243c6d92add73e6
VitalyKorzin <72571981+VitalyKorzin@users.noreply.github.com>
2022-06-28 04:23:11 +0300

    Refactored

--- Logging.cs
+++ Logging.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 
 public static class Logging
 {
     public static void Main()
     {
-        PathFinder pathFinder_1 = new PathFinder(ChainOfLogging.Create(new FileLogger()));
-        PathFinder pathFinder_2 = new PathFinder(ChainOfLogging.Create(new ConsoleLogger()));
-        PathFinder pathFinder_3 = new PathFinder(ChainOfLogging.Create(new SecureLogger(new FileLogger())));
-        PathFinder pathFinder_4 = new PathFinder(ChainOfLogging.Create(new SecureLogger(new ConsoleLogger())));
-        PathFinder pathFinder_5 = new PathFinder(ChainOfLogging.Create(new ConsoleLogger(), new SecureLogger(new FileLogger())));
+        PathFinder pathFinder_1 = new PathFinder(new FileLogger());
+        PathFinder pathFinder_2 = new PathFinder(new ConsoleLogger());
+        PathFinder pathFinder_3 = new PathFinder(new SecureLogger(new FileLogger(), new FridayLoggingPolicy()));
+        PathFinder pathFinder_4 = new PathFinder(new SecureLogger(new ConsoleLogger(), new FridayLoggingPolicy())));
+        PathFinder pathFinder_5 = new PathFinder(new ConsoleLogger(), new SecureLogger(new FileLogger(), new FridayLoggingPolicy()));
         pathFinder_1.Find();
         pathFinder_2.Find();
         pathFinder_3.Find();
@@ -51,56 +52,57 @@ public class FileLogger : ILogger
 public class SecureLogger : ILogger
 {
     private readonly ILogger _logger;
+    private readonly ILoggingPolicy _loggingPolicy;
 
-    public SecureLogger(ILogger logger)
+    public SecureLogger(ILogger logger, ILoggingPolicy loggingPolicy)
     {
         if (logger == null)
-            throw new ArgumentNullException();
+            throw new ArgumentNullException(nameof(logger));
+
+        if (loggingPolicy == null)
+            throw new ArgumentNullException(nameof(loggingPolicy));
 
         _logger = logger;
+        _loggingPolicy = loggingPolicy;
     }
 
     public void WriteError(string message)
     {
-        if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+        if (_loggingPolicy.CanWriteError())
             _logger.WriteError(message);
     }
 }
 
-public class ChainOfLogging
+public interface ILoggingPolicy
 {
-    private readonly IEnumerable<ILogger> _loggers;
+    bool CanWriteError();
+}
 
-    public ChainOfLogging(IEnumerable<ILogger> loggers)
-    {
-        if (loggers == null)
-            throw new ArgumentNullException();
-
-        _loggers = loggers;
-    }
-
-    public void WriteError(string message)
-    {
-        foreach (var logger in _loggers)
-            logger.WriteError(message);
-    }
-
-    public static ChainOfLogging Create(params ILogger[] loggers)
-        => new ChainOfLogging(loggers);
+public class FridayLoggingPolicy
+{
+    public bool CanWriteError() 
+        => DateTime.Now.DayOfWeek == DayOfWeek.Friday
 }
 
 public class PathFinder
 {
     private readonly string _message = "...";
-    private readonly ChainOfLogging _chain;
+    private readonly ILogger[] _loggers;
 
-    public PathFinder(ChainOfLogging chain)
+    public PathFinder(params ILogger[] loggers)
     {
-        if (chain == null)
-            throw new ArgumentNullException();
+        if (loggers == null)
+            throw new ArgumentNullException(nameof(loggers));
 
-        _chain = chain;
+        if (loggers.Length == 0)
+            throw new ArgumentOutOfRangeException(nameof(loggers));
+
+        _loggers = loggers;
     }
 
-    public void Find() => _chain.WriteError(_message);
+    public void Find()
+    {
+        foreach (var logger in _loggers)
+            logger.WriteError(_message);
+    }
 }
